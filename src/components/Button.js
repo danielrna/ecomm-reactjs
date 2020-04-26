69979b86e14a4af1db0e3e119068a966373716cc
danielrna <nigga77@live.fr>
2020-04-26 22:54:52 +0200

    css

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
     text-transform: capitalize;
-    font-size: 1.4rem;
+    font-size: 1.0rem;
     background:transparent;
     border: 0.05rem solid var(--lightBlue);
     border-color: ${props =>
@@ -17,8 +17,7 @@ export const ButtonContainer = styled.button`
     margin: 0.2rem 0.5rem 0.2rem 0;
     transition: all 0.5s ease-in-out;
     &:hover{
-    background: ${prop =>
-        prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"}
+    border-color:white;
     color: var(--mainBlue);
     }
     &:focus{
